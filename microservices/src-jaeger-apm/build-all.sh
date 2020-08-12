f2d1d52e0f4e8b8a736e0a3c8f8045cadb80ac22
heronimus <heronimustra@gmail.com>
2020-08-12 16:14:55 +0700

    Jaeger APM: (Update Java Client & ENV VAR)

--- microservices/src-jaeger-apm/build-all.sh
+++ microservices/src-jaeger-apm/build-all.sh
@@ -14,14 +14,14 @@ echo -e "\n\n--> Build: Backend Random Cat .. \n"
 cd ../app-backend-random-cat
 docker build -t heronimus/apm-app-randomcat:0.0.1-jaegerapm .
 
-# echo -e "\n\n--> Build: Frontend Home UI .. \n"
-# cd ../app-frontend-home-ui
-# docker build -t heronimus/apm-app-homeui:0.0.1-jaegerapm .
-#
-#
-# echo -e "\n\n--> Successfully build all demo microservices .. \n"
-# docker image ls | grep "jaegerapm"
-#
+echo -e "\n\n--> Build: Frontend Home UI .. \n"
+cd ../app-frontend-home-ui
+docker build -t heronimus/apm-app-homeui:0.0.1-jaegerapm .
+
+
+echo -e "\n\n--> Successfully build all demo microservices .. \n"
+docker image ls | grep "jaegerapm"
+
 # echo -e "\n\n--> Push update \n"
 # docker push heronimus/apm-app-cloudgateway:0.0.1-jaegerapm
 # docker push heronimus/apm-app-userservices:0.0.1-jaegerapm
